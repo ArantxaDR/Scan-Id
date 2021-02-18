ffb8df51e7e706b0195b18e8c6587b2b4d897eec
ArantxaDR <arantxaderuiz@gmail.com>
2021-02-18 15:06:54 +0100

    create components

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+//components
+import Header from "./Header";
+import ScanEmpty from "./ScanEmpty";
+import "../styles/App.scss";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <ScanEmpty />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/Header.scss";
+
+export default function Header() {
+  return (
+    <header className="bank-client">
+      <h1>BankClient</h1>
+    </header>
+  );
+}
