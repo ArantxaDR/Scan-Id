99101d06bb61cd5a867dd095a254ea4adb36dd63
ArantxaDR <arantxaderuiz@gmail.com>
2021-02-20 11:48:11 +0100

    testing camera

--- src/components/TakePhoto.tsx
+++ src/components/TakePhoto.tsx
@@ -1,8 +1,8 @@
 import React, { useRef, useState, useCallback, useEffect } from "react";
 import WebCam from "react-webcam";
+import api from "../services/Api";
 import { Link } from "react-router-dom";
 import alert from "../styles/assets/Alert.svg";
-
 
 const TakePhoto = (props: any) => {
   const [borderPictureCss, setBorderPictureCss] = useState<string>("");
@@ -29,4 +29,48 @@ const TakePhoto = (props: any) => {
       }
     }, 1000);
   }, [webcamRef, props.setImgSrc]);
+
+  const validatePhoto = (intervalId: ReturnType<typeof setInterval>) => {
+    api().then((data: string) => {
+      if (data === "Approved") {
+        clearInterval(intervalId);
+        setBorderPictureCss("approved");
+        props.setPictureStatus("approved");
+        props.history.push("/");
+      } else {
+        setBorderPictureCss("rejected");
+        props.setPictureStatus("rejected");
+        props.setBtnMessage("Retake picture");
+      }
+    });
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="container-title">Take picture</h1>
+        <p className="container-title__text">
+          Fit your ID card inside the frame. The picture will be taken
+          automatically.
+        </p>
+        <WebCam
+          className={borderPictureCss}
+          audio={false}
+          height={160}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          width={260}
+          videoConstraints={videoConstraints}
+        />
+        <p className="camera-container__alert">
+          <img src={alert} alt="Icon" />
+          Room lighting is too low
+        </p>
+        <Link to="/" className=" cancel " title="Home l">
+          Cancel
+        </Link>
+      </div>
+    </>
+  );
+};
 export default TakePhoto;
